b4767aa7c6bdb10a379a39c4872c67690367b596
fqr1d0 <farid.adoi@epitech.eu>
2026-05-25 02:24:30 +0100

    Fix netlyfy deployment error by adding right dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'server',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
   }
